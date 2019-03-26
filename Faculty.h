d8e63c2b4ab7c42a4f42fb496593970ce0c530c0
Giralda <48957798+Giralda@users.noreply.github.com>
2019-03-26 11:25:40 -0400

    Create Faculty.h

---
+++ Faculty.h
@@ -0,0 +1,14 @@
+#ifndef FACULTY
+#define FACULTY
+#include <string>
+
+struct Faculty
+{
+	std::string name;
+	int ID;
+	std::string address;
+	std::string dob;
+	std::string email;
+};
+
+#endif // !Faculty
