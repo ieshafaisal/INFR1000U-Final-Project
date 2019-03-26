76fdde8f8231fead98c58737eef68f44022001a0
ieshafaisal <48957857+ieshafaisal@users.noreply.github.com>
2019-03-26 15:43:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@
 
 int main()
 {
+	struct Student s1 = { "Zack Smith", 773456 , "135 Mooncreek Ave, Oshawa", "5-May-1998" , "Science" , "zacksmith78@gmail.com" };
+	struct Student s2 = { "William Jones", 773457 , "18 Oshawa Rd, Oshawa", "8-August-1997" , "Business" , "williamjones91@gmail.com" };
+	struct Student s3 = { "Ross Morgan", 773458 , "156 Mooncreek Ave, Oshawa", "19-November-1998" , "Business" , "rossmorgan@gmail.com" };
+	struct Student s4 = { "Zhang Wo", 773459 , "14 Riverside Rd, Oshawa", "14-September-1999" , "Science" , "zhangwo99@gmail.com" };
+	struct Student s5 = { "Nancy Cooper", 773460 , "2336 Newside Rd, Oshawa", "10-May-2000" , "Information Technology" , "nancycooper2000@gmail.com" }; 
+	
 	char n, k, ws;
 	unsigned int x = 0;
 	std::vector<Student>students;
