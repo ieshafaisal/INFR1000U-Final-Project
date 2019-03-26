5e5820d9119f5f7069f6d177c24e12f1fe6f9da4
Giralda <48957798+Giralda@users.noreply.github.com>
2019-03-26 11:25:18 -0400

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,15 @@
+#ifndef STUDENT
+#define STUDENT
+#include <string>
+
+struct Student
+{
+	std::string name;
+	int ID;
+	std::string address;
+	std::string dob;
+	std::string department;
+	std::string email;
+};
+
+#endif // !Student
